76eec9aca21f4c73d1ab808887d91cbb5d145854
Casey Baggz <caseybaggz@gmail.com>
2023-06-09 21:22:52 -0500

    feat: add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,43 @@
+const ERROR = 2
+const WARN = 1
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+    // this must be last
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['import'],
+  rules: {
+    'import/extensions': [ERROR, 'always', { ignorePackages: true }],
+    'import/no-unresolved': [2, { ignore: ['solid-start/*', '~/server/*'] }],
+    'import/order': [
+      WARN,
+      {
+        groups: [
+          'builtin',
+          ['external', 'internal'],
+          'parent',
+          ['sibling', 'index'],
+        ],
+      },
+    ],
+  },
+  settings: {
+    'import/ignore': ['node_modules', '.json$', '.(scss|less|css|styl)$'],
+    'import/resolver': 'node',
+  },
+}
