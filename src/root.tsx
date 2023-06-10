c83bd52fe9ff9d9822939d2e58ac502bb86ab99e
Casey Baggz <caseybaggz@gmail.com>
2023-06-09 20:52:14 -0500

    feat(assets): add fonts

--- src/root.tsx
+++ src/root.tsx
@@ -13,16 +13,19 @@ import {
   Scripts,
   Title,
 } from "solid-start";
+
+import './font.css';
 import "./root.css";
 
 export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>SolidStart + AuthJS</Title>
+        <Title>Virtual DnD</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+
       <Body>
         <Suspense>
           <SessionProvider>
