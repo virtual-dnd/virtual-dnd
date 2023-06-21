16aea7e3ee06c6eb06d99bff8b19a23d406dd5c9
Casey Baggz <caseybaggz@gmail.com>
2023-06-20 19:25:08 -0500

    feat(db): update SB options to use pkce flow

--- src/lib/solidbaseClient.ts
+++ src/lib/solidbaseClient.ts
@@ -2,5 +2,11 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   `https://${import.meta.env.VITE_SUPABASE_ID}.supabase.co`,
-  import.meta.env.VITE_SUPABASE_KEY as string
+  import.meta.env.VITE_SUPABASE_KEY as string,
+  {
+    auth: {
+      flowType: 'pkce',
+      // storage: TODO: add custom cookie storage here
+    },
+  }
 )
