4a5a0800dcd765bed00b7eb4fc9cd92607e27a58
Casey Baggz <caseybaggz@gmail.com>
2023-06-22 18:48:52 -0500

    feat(auth): add new API routes for sessions

--- src/db/session.ts
+++ src/db/session.ts
@@ -1,45 +1,13 @@
 import { redirect } from 'solid-start/server'
 import { createCookieSessionStorage } from 'solid-start/session'
+import { type Provider } from '@supabase/supabase-js'
 import { supabase } from '~/lib/solidbaseClient.ts'
-
-// type SigninForm = {
-//   email: string
-// }
-
-// TODO: Might not need this
-
-// export async function register({ username, password }: LoginForm) {
-//   return db.user.create({
-//     data: { username: username, password },
-//   })
-// }
-
-// export async function login() {
-//   const user = await db.user.findUnique({ where: { username } })
-//   if (!user) return null
-//   const isCorrectPassword = password === user.password
-//   if (!isCorrectPassword) return null
-//   return user
-// }
-
-export async function logout(request: Request) {
-  const session = await storage.getSession(request.headers.get('Cookie'))
-  const { error } = await supabase.auth.signOut()
-
-  if (error) throw error
-
-  return redirect('/login', {
-    headers: {
-      'Set-Cookie': await storage.destroySession(session),
-    },
-  })
-}
 
 const sessionSecret = import.meta.env.VITE_SESSION_SECRET
 
 export const storage = createCookieSessionStorage({
   cookie: {
-    name: 'VDND_session',
+    name: 'VDnD_session',
     // secure doesn't work on localhost for Safari
     // https://web.dev/when-to-use-local-https/
     secure: import.meta.env.PROD,
@@ -74,16 +42,18 @@ export async function getUserId(request: Request) {
   return userId
 }
 
-export async function requireUserId(
+export async function requireUserSession(
   request: Request,
   redirectTo: string = new URL(request.url).pathname
 ) {
   const session = await getUserSession(request)
   const userId = session.get('userId')
+
   if (!userId || typeof userId !== 'string') {
     const searchParams = new URLSearchParams([['redirectTo', redirectTo]])
     throw redirect(`/login?${searchParams}`)
   }
+
   return userId
 }
 
@@ -99,6 +69,70 @@ export async function getUser(request: Request) {
     } = await supabase.auth.getUser(userId)
     return user
   } catch {
-    throw logout(request)
+    throw signOut(request)
   }
+}
+
+// helpers
+
+// type SigninForm = {
+//   email: string
+// }
+
+// export async function register({ username, password }: LoginForm) {
+//   return db.user.create({
+//     data: { username: username, password },
+//   })
+// }
+
+// export async function login() {
+//   const user = await db.user.findUnique({ where: { username } })
+//   if (!user) return null
+//   const isCorrectPassword = password === user.password
+//   if (!isCorrectPassword) return null
+//   return user
+// }
+
+export async function signInWithMagicLink(email: string) {
+  const { data, error } = await supabase.auth.signInWithOtp({
+    email,
+    options: {
+      emailRedirectTo: `${import.meta.env.BASE_URL}/api/auth/callback`,
+    },
+  })
+
+  if (error) throw error
+
+  return data.user
+}
+
+export async function verifyMagicLink(code: string) {
+  const { data, error } = await supabase.auth.exchangeCodeForSession(code)
+
+  console.log('veryfyMagicLink', data)
+
+  if (error) throw error
+
+  return redirect('/signin')
+}
+
+export async function signInWithProvider(provider: Provider, request: Request) {
+  const { data, error } = await supabase.auth.signInWithOAuth({ provider })
+
+  console.log('signInWithProvider', data)
+
+  if (error) throw error
+}
+
+export async function signOut(request: Request) {
+  const session = await storage.getSession(request.headers.get('Cookie'))
+  const { error } = await supabase.auth.signOut()
+
+  if (error) throw error
+
+  return redirect('/signin', {
+    headers: {
+      'Set-Cookie': await storage.destroySession(session),
+    },
+  })
 }
--- src/routes/signin/index.tsx
+++ src/routes/signin/index.tsx
@@ -1,32 +1,54 @@
-// TODO: Add sign in with Google and Discord
+import { createServerAction$ } from 'solid-start/server'
+import { signInWithMagicLink, signInWithProvider } from '~/db/session.ts'
 
 export default function Signin() {
+  const [, { Form }] = createServerAction$(
+    async (form: FormData, { request }) => {
+      const intent = form.get('intent') as string
+
+      switch (intent) {
+        case 'magicLink':
+          return signInWithMagicLink(form.get('email') as string, request)
+
+        case 'discord':
+        case 'github':
+          return signInWithProvider(intent, request)
+
+        default:
+          throw new Error(
+            `Unknown intent: ${intent}. This should never happen.`
+          )
+      }
+    }
+  )
+
   return (
     <div>
       <h1>Sign In</h1>
 
       <div>
-        <form>
+        <Form>
           <label html-for="email">Email</label>
+          <input type="hidden" name="intent" value="magicLink" />
           <input type="email" id="email" name="email" />
           <button type="submit">Sign in</button>
-        </form>
+        </Form>
       </div>
 
       <div>
-        <form>
-          <button type="submit" value="google">
-            Sign in with Google
-          </button>
-        </form>
+        <Form>
+          <input type="hidden" name="intent" value="github" />
+          <button type="submit">Sign in with Google</button>
+        </Form>
       </div>
 
       <div>
-        <form>
-          <button type="submit" value="discord">
+        <Form>
+          <input type="hidden" name="intent" value="discord" />
+          <button name="intent" type="submit">
             Sign in with Discord
           </button>
-        </form>
+        </Form>
       </div>
     </div>
   )
