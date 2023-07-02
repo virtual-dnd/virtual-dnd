186a4784bed02cfde12ec0bffe1b93de6171df8f
Casey Baggz <caseybaggz@gmail.com>
2023-07-01 19:52:43 -0500

    feat(me): add Update & basic avatar upload

--- src/db/profiles.ts
+++ src/db/profiles.ts
@@ -15,30 +15,75 @@ export async function getUserProfile(request: Request) {
   return { data: data[0], headers: response.headers }
 }
 
-export async function updateUserProfile(formData: FormData, request: Request) {
+// UPDATE
+
+export async function updateUserAvatar(
+  payload: { avatar: File | null },
+  request: Request
+) {
   const response = new Response()
   const serverSupabase = createServerClient(
     import.meta.env.VITE_SUPABASE_URL,
     import.meta.env.VITE_SUPABASE_KEY,
     { request, response }
   )
+  const { avatar } = payload
 
-  const {
-    data: { session },
-    error,
-  } = await serverSupabase.auth.getSession()
+  if (avatar?.name) {
+    const { data, error } = await serverSupabase.storage
+      .from('avatars')
+      .upload(`users/${avatar?.name}`, avatar, {
+        cacheControl: '3600',
+        upsert: false,
+      })
+
+    if (error) throw error
+
+    return { data, headers: response.headers }
+  }
+
+  return { data: null, headers: response.headers }
+}
+
+export async function updateUserProfile(
+  payload: UserProfileForm,
+  request: Request
+) {
+  const response = new Response()
+  const serverSupabase = createServerClient(
+    import.meta.env.VITE_SUPABASE_URL,
+    import.meta.env.VITE_SUPABASE_KEY,
+    { request, response }
+  )
+  const { id, avatar, ...profileForm } = payload
+
+  const { data: avatarData } = await updateUserAvatar({ avatar }, request)
+
+  const { data, error } = await serverSupabase
+    .from('profiles')
+    .update({
+      ...profileForm,
+      avatar: avatarData?.path ?? null,
+    })
+    .eq('id', id)
+    .select()
 
   if (error) throw error
 
-  return { session, headers: response.headers }
+  return { data, headers: response.headers }
 }
 
 // types
 
-export interface User {
-  id: string
-  name: string
-  email: string
-  pronouns: string
+export interface UserProfileForm extends Omit<UserProfile, 'avatar'> {
+  avatar: File | null
+}
+
+export interface UserProfile {
   avatar: string
+  display_name: string
+  email?: string
+  id: string
+  user_name: string
+  pronouns: string
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,8 @@ export default {
   theme: {
     extend: {
       animation: {
+        'bounce-in-from-bottom':
+          'bounceInFromBottom 300ms cubic-bezier(0.5, 1, 0.89, 1) forwards',
         'grow-radius': 'growRadius 250ms ease-in-out forwards',
       },
       keyframes: {
@@ -17,74 +19,86 @@ export default {
           '0%': { borderRadius: '50%' },
           '100%': { borderRadius: '33%' },
         },
+        bounceInFromBottom: {
+          '0%': {
+            opacity: 0,
+            transform: 'translateY(100%)',
+          },
+          '75%': {
+            transform: 'translateY(-20%)',
+          },
+          '100%': {
+            opacity: 1,
+            transform: 'translateY(0)',
+          },
+        },
       },
-    },
-
-    colors: {
-      // default
-      background: '#414eed',
-      border: '#b6b8be',
-      'surface-100': WHITE,
-      'surface-200': '#f2f3f5',
-      'surface-300': '#ebedef',
-      'surface-400': '#e3e5e8',
-      'surface-inverse': '#060607',
-      'text-100': '#4e5058',
-      'text-200': '#060607',
-      'text-inverse': WHITE,
-      // action
-      'action-text': WHITE,
-      'action-text-100': SURFACE_100,
-      'action-link-text': '#006ce7',
-      'action-background': '#5764f2',
-      'action-background-hover': '#2a3aef',
-      'action-secondary-background': '#6d6f78',
-      'action-secondary-background-hover': '#55565e',
-      'action-secondary-text': WHITE,
-      // info
-      'info-background': '#EB459E',
-      // success
-      'success-background': '#23a559',
-      'success-text': WHITE,
-      // warning
-      'warning-background': '#FEE75C',
-      // danger
-      'danger-background': '#da373c',
-      'danger-background-hover': '#9b1c21',
-      'danger-text': WHITE,
-
-      darkMode: {
+      colors: {
         // default
         background: '#414eed',
-        border: '#3c3d44',
-        'surface-100': SURFACE_100,
-        'surface-200': '#2b2d30',
-        'surface-300': '#232428',
-        'surface-400': '#1e1f22',
-        'surface-inverse': '#dcddde',
-        'text-100': WHITE,
-        'text-200': '#b5bac1',
-        'text-inverse': SURFACE_100,
+        border: '#b6b8be',
+        'surface-100': WHITE,
+        'surface-200': '#f2f3f5',
+        'surface-300': '#ebedef',
+        'surface-400': '#e3e5e8',
+        'surface-inverse': '#060607',
+        'text-100': '#4e5058',
+        'text-200': '#060607',
+        'text-inverse': WHITE,
         // action
         'action-text': WHITE,
         'action-text-100': SURFACE_100,
-        'action-link-text': '#09a7fc',
+        'action-link-text': '#006ce7',
         'action-background': '#5764f2',
         'action-background-hover': '#2a3aef',
-        'action-secondary-background': SURFACE_100,
-        'action-secondary-background-hover': '#3d3c62',
+        'action-secondary-background': '#6d6f78',
+        'action-secondary-background-hover': '#55565e',
         'action-secondary-text': WHITE,
         // info
         'info-background': '#EB459E',
         // success
-        'success-background': '#257f46',
+        'success-background': '#23a559',
         'success-text': WHITE,
         // warning
         'warning-background': '#FEE75C',
         // danger
         'danger-background': '#da373c',
-        'danger-background-hover': '#b8191d',
+        'danger-background-hover': '#9b1c21',
         'danger-text': WHITE,
+
+        darkMode: {
+          // default
+          background: '#414eed',
+          border: '#3c3d44',
+          'surface-100': SURFACE_100,
+          'surface-200': '#2b2d30',
+          'surface-300': '#232428',
+          'surface-400': '#1e1f22',
+          'surface-inverse': '#dcddde',
+          'text-100': WHITE,
+          'text-200': '#b5bac1',
+          'text-inverse': SURFACE_100,
+          // action
+          'action-text': WHITE,
+          'action-text-100': SURFACE_100,
+          'action-link-text': '#09a7fc',
+          'action-background': '#5764f2',
+          'action-background-hover': '#2a3aef',
+          'action-secondary-background': SURFACE_100,
+          'action-secondary-background-hover': '#3d3c62',
+          'action-secondary-text': WHITE,
+          // info
+          'info-background': '#EB459E',
+          // success
+          'success-background': '#257f46',
+          'success-text': WHITE,
+          // warning
+          'warning-background': '#FEE75C',
+          // danger
+          'danger-background': '#da373c',
+          'danger-background-hover': '#b8191d',
+          'danger-text': WHITE,
+        },
       },
     },
 
